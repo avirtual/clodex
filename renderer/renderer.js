9821056b01555c4f551f40d37f44ee2b62d06103
Bogdan Ionescu <bogdan.ionescu@gmail.com>
2026-09-11 04:59:06 +0300

    t812 r1: restore snapAccount on the restart rebuild (swept in from a red-proof revert); rebuild web-dist

--- renderer/renderer.js
+++ renderer/renderer.js
@@ -701,7 +701,7 @@ function restartSessionWithReattach(name) {
     }
     if (snapType) {
       createTerminal(name);
-      addSessionToSidebar(name, snapType, snapCwd, null, res.backend ?? snapBackend, snapTeam, snapNoWire);
+      addSessionToSidebar(name, snapType, snapCwd, null, res.backend ?? snapBackend, snapTeam, snapNoWire, snapAccount);
       switchSession(name);
     }
   });
